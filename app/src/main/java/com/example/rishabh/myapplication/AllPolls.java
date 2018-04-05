ce5448b5aee5f5f4709d795d20631453033becdd
Rishabh3095 <rishabh.gupta@sjsu.edu>
2018-04-05 00:25:11 -0700

    Working UI 5

---
+++ app/src/main/java/com/example/rishabh/myapplication/AllPolls.java
@@ -0,0 +1,13 @@
+package com.example.rishabh.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllPolls extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_polls);
+    }
+}
---
+++ app/src/main/java/com/example/rishabh/myapplication/AllRatings.java
@@ -0,0 +1,13 @@
+package com.example.rishabh.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllRatings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_ratings);
+    }
+}
