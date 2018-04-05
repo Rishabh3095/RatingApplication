c2fff4373c379b8df60ddd5c10c03b46e994d445
Rishabh3095 <rishabh.gupta@sjsu.edu>
2018-04-04 18:54:34 -0700

    Working UI 3

--- app/src/main/java/com/example/rishabh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rishabh/myapplication/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.rishabh.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button logIn;
+    Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logIn = (Button) findViewById(R.id.button_login);
+        signUp = (Button) findViewById(R.id.button_sign_up);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainMenu.class));
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+            }
+        });
     }
 }
