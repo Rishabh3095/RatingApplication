50c385547c4151f46d303817e2129b513307378f
EmersonYe <emersonye@gmail.com>
2018-04-12 19:10:45 -0700

    RatingActivity now returns to main menu instead of last page to avoid having to uneccesarily update AllRatings

--- app/src/main/java/com/example/rishabh/myapplication/RatingActivity.java
+++ app/src/main/java/com/example/rishabh/myapplication/RatingActivity.java
@@ -1,5 +1,6 @@
 package com.example.rishabh.myapplication;
 
+import android.content.Intent;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -105,6 +106,6 @@ public class RatingActivity extends AppCompatActivity
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connect.createRatingRate(User.get().getID(),ratingID,Integer.toString(ratingSeekBar.getProgress()));
-        finish();
+        startActivity(new Intent(RatingActivity.this, Menu.class));
     }
 }
