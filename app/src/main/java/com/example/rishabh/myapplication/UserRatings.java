5d407b5860f902e8ee3d18d7c94d58b5d5754ba9
EmersonYe <emersonye@gmail.com>
2018-05-08 20:29:36 -0700

    Implement view user ratings

--- app/src/main/java/com/example/rishabh/myapplication/UserRatings.java
+++ app/src/main/java/com/example/rishabh/myapplication/UserRatings.java
@@ -1,6 +1,7 @@
 package com.example.rishabh.myapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,72 +20,71 @@ import java.util.HashMap;
 
 import static com.example.rishabh.myapplication.Connect.TAG_DATE;
 import static com.example.rishabh.myapplication.Connect.TAG_MAX_RATE;
+import static com.example.rishabh.myapplication.Connect.TAG_RATING_ID;
 import static com.example.rishabh.myapplication.Connect.TAG_TITLE;
 
-public class UserRatings extends AppCompatActivity {
+public class UserRatings extends AppCompatActivity
+{
 
-    TextView userRatingHeading;
-    Button userRatingBack;
-    Button userRatingAllRatings;
-    private ListView mListView;
+    public static final String TAG_RATING_TITLE = "rating_id";
     private ArrayAdapter<String> mAdapter;
     private ArrayList<String> ratingTitles;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_ratings);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         ratingTitles = new ArrayList<>();
 
-        /*mListView = (ListView) findViewById(R.id.listview_all_polls);
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, updateRatingTitles());
-        mListView.setAdapter(mAdapter);*/
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ListView mListView = (ListView) findViewById(R.id.listview_user_ratings);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ratingTitles);
+        mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        userRatingHeading = (TextView) findViewById(R.id.user_rating_heading);
-        userRatingBack = (Button) findViewById(R.id.user_ratings_back);
-        userRatingAllRatings = (Button) findViewById(R.id.user_rating_to_all_ratings);
-
-        userRatingBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UserRatings.this, Menu.class));
-            }
-        });
-
-        userRatingAllRatings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UserRatings.this, AllRatings.class));
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                Intent intent = new Intent(UserRatings.this, RatingActivity.class);
+                String ratingID = ((String) adapterView.getItemAtPosition(position)).substring(0, ((String) adapterView.getItemAtPosition(position)).indexOf(" |"));
+                intent.putExtra(TAG_RATING_TITLE, ratingID);
+                startActivity(intent);
             }
         });
     }
-    private ArrayList<String> updateRatingTitles() {
-        // Terrible workaround to avoid NetworkOnMainThreadException
-        // TODO: move Connect.getAllPolls() to non-UI thread
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
 
-        ratingTitles.clear();
-        for(HashMap<String, String> hashMap : Connect.getUserRatings(User.get().getID()))
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        new getRatingDataTask().execute();
+    }
+
+    private class getRatingDataTask extends AsyncTask<Void, Void, ArrayList<String>>
+    {
+        @Override
+        protected ArrayList<String> doInBackground(Void... voids)
         {
-            // TODO: retrieve rating and display instead of placeholder text
-            String formatted = String.format("%s | %10.10s | %s",hashMap.get(TAG_TITLE), hashMap.get(TAG_DATE), "RATING/" + hashMap.get(TAG_MAX_RATE));
-            ratingTitles.add(formatted);
+            ArrayList<String> strings = new ArrayList<>();
+            ArrayList<HashMap<String, String>> userRatings = Connect.getUserRatings(User.get().getID());
+            if (userRatings == null) {
+                strings.add("You have not submitted any ratings. You should submit one!");
+            } else {
+                for (HashMap<String, String> hashMap : userRatings) {
+                    int ratingRate = Connect.getRatingRate(hashMap.get(TAG_RATING_ID));
+                    String formatted = String.format("%s | %s | %10.10s | %s", hashMap.get(TAG_RATING_ID), hashMap.get(TAG_TITLE), hashMap.get(TAG_DATE), ratingRate + "/" + hashMap.get(TAG_MAX_RATE));
+                    strings.add(formatted);
+                }
+            }
+            return strings;
         }
-        return ratingTitles;
 
+        @Override
+        protected void onPostExecute(ArrayList<String> strings)
+        {
+            ratingTitles.clear();
+            ratingTitles.addAll(strings);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
