849c6ccf13c830ae2e5d5a4081792cbffa2417bc
EmersonYe <emersonye@gmail.com>
2018-05-10 17:49:06 -0700

    Implement user polls

--- app/src/main/java/com/example/rishabh/myapplication/AllPolls.java
+++ app/src/main/java/com/example/rishabh/myapplication/AllPolls.java
@@ -5,9 +5,13 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,6 +25,7 @@ public class AllPolls extends AppCompatActivity {
 
     private ArrayAdapter<String> mAdapter;
     private ArrayList<String> pollTitles = new ArrayList<>();
+    String searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,6 +33,28 @@ public class AllPolls extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_polls);
         ListView mListView = (ListView) findViewById(R.id.listview_all_polls);
+        final EditText searchField = (EditText) findViewById(R.id.search_edit_text);
+        searchText = searchField.getText().toString();
+        searchField.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+                searchText = searchField.getText().toString();
+                new getRatingDataTask().execute();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+            }
+        });
 
         mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pollTitles);
         mListView.setAdapter(mAdapter);
@@ -59,10 +86,16 @@ public class AllPolls extends AppCompatActivity {
         {
             ArrayList<String> strings = new ArrayList<>();
             ArrayList<HashMap<String, String>> allPolls = Connect.getAllPolls();
-            if (allPolls == null) {
+            ArrayList<HashMap<String, String>> searchPolls = new ArrayList<>();
+            for(HashMap<String, String> hashMap : allPolls){
+                if(hashMap.get(TAG_TITLE).contains(searchText)){
+                    searchPolls.add(hashMap);
+                }
+            }
+            if (searchPolls.isEmpty()) {
                 strings.add("No polls have been submitted. You should submit one!");
             } else {
-                for (HashMap<String, String> hashMap : allPolls) {
+                for (HashMap<String, String> hashMap : searchPolls) {
                     String formatted = String.format("%s | %s | %10.10s", hashMap.get(TAG_POLL_ID), hashMap.get(TAG_TITLE), hashMap.get(TAG_DATE));
                     strings.add(formatted);
                 }
--- app/src/main/java/com/example/rishabh/myapplication/UserPolls.java
+++ app/src/main/java/com/example/rishabh/myapplication/UserPolls.java
@@ -1,87 +1,89 @@
 package com.example.rishabh.myapplication;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 import static com.example.rishabh.myapplication.Connect.TAG_DATE;
+import static com.example.rishabh.myapplication.Connect.TAG_POLL_ID;
+import static com.example.rishabh.myapplication.Connect.TAG_SJSUID;
 import static com.example.rishabh.myapplication.Connect.TAG_TITLE;
 
 public class UserPolls extends AppCompatActivity {
 
-    TextView userPollsHeading;
-    Button userPollToMenu;
-    Button userPollAllPolls;
-    private ListView mListView;
     private ArrayAdapter<String> mAdapter;
-    private ArrayList<String> pollTitles;
+    private ArrayList<String> pollTitles = new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_polls);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ListView mListView = (ListView) findViewById(R.id.listview_user_polls);
 
-        userPollsHeading = (TextView) findViewById(R.id.user_polls);
-        userPollToMenu = (Button) findViewById(R.id.user_polls_back);
-        userPollAllPolls = (Button) findViewById(R.id.user_polls_to_all_polls);
-
-        pollTitles = new ArrayList<>();
-
-        //mListView = (ListView) findViewById(R.id.listview_all_polls);
-        //mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, updatePollTitles());
-       // mListView.setAdapter(mAdapter);
-
-        userPollToMenu.setOnClickListener(new View.OnClickListener() {
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pollTitles);
+        mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UserPolls.this, Menu.class));
-            }
-        });
-
-        userPollAllPolls.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UserPolls.this, AllPolls.class));
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                Intent intent = new Intent(UserPolls.this, PollActivity.class);
+                String pollID = ((String) adapterView.getItemAtPosition(position)).substring(0, ((String) adapterView.getItemAtPosition(position)).indexOf(" |"));
+                intent.putExtra(TAG_POLL_ID, pollID);
+                intent.putExtra(TAG_TITLE,pollTitles.get(position));
+                startActivity(intent);
             }
         });
     }
 
-    private ArrayList<String> updatePollTitles() {
-        // Terrible workaround to avoid NetworkOnMainThreadException
-        // TODO: move Connect.getAllPolls() to non-UI thread
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        new getRatingDataTask().execute();
+    }
 
-        pollTitles.clear();
-        /*for(HashMap<String, String> hashMap : Connect.getUserPolls(User.get().getID()))
+    private class getRatingDataTask extends AsyncTask<Void, Void, ArrayList<String>>
+    {
+        @Override
+        protected ArrayList<String> doInBackground(Void... voids)
         {
-            // TODO: retrieve rating and display instead of placeholder text
-            String formatted = String.format("%s | %10.10s",hashMap.get(TAG_TITLE), hashMap.get(TAG_DATE));
-            pollTitles.add(formatted);
-        }*/
-        return pollTitles;
+            ArrayList<String> strings = new ArrayList<>();
+            ArrayList<HashMap<String, String>> userPolls = new ArrayList<>();
+            for( HashMap<String, String> hashMap: Connect.getAllPolls()){
+                if(Objects.equals(hashMap.get(TAG_SJSUID), User.get().getID())) {
+                    userPolls.add(hashMap);
+                }
+            }
+            if (userPolls == null) {
+                strings.add("No polls have been submitted. You should submit one!");
+            } else {
+                for (HashMap<String, String> hashMap : userPolls) {
+                    String formatted = String.format("%s | %s | %10.10s", hashMap.get(TAG_POLL_ID), hashMap.get(TAG_TITLE), hashMap.get(TAG_DATE));
+                    strings.add(formatted);
+                }
+            }
+            return strings;
+        }
 
-}}
+        @Override
+        protected void onPostExecute(ArrayList<String> strings)
+        {
+            pollTitles.clear();
+            pollTitles.addAll(strings);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+}
